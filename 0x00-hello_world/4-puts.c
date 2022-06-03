e7bc452f5ae478e9512ff9d0e6709fe905e70ea9
yabsera <kidistkifleyohannes364@gmail.com>
2022-06-02 17:21:00 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+nclude <stdio.h>
+
+/**
+ *   *  * main - Entry point
+ *   *   *
+ *   *    *Return:Always 0 (Success)
+ *   **/
+int main(void)
+{
+               puts("\"Alx low level programming);
+                         
+                          return (0);
+}	   		  
