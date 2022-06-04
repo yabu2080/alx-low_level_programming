aced3f4e86d3a7db229dc0a426f5185d8c187525
yabsera <kidistkifleyohannes364@gmail.com>
2022-06-04 13:09:07 -0700

    lol

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,4 @@
-nclude < stdio.h >
+nclude <stdio.h>
 
 /*main -> assign a random number to the variable n each time it is executed*/
 /*and print the last digit of the number stored in the variable n*/
