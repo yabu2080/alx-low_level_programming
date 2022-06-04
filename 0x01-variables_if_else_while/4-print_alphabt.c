809334023bb71879a2dff549bc8a5392596622d4
yabsera <kidistkifleyohannes364@gmail.com>
2022-06-04 13:07:28 -0700

    aplfour

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include < stdio.h >
+#include <stdio.h>
 
 /*main -> assign a random number to the variable n each time it is executed*/
 /*and print the last digit of the number stored in the variable n*/
