39fcf8c3161d1baee209780f6e92937ca92ad266
yabsera <kidistkifleyohannes364@gmail.com>
2022-06-04 13:53:30 -0700

    la

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  *  *main -> assign a random number to the variable n each time it is executed
  *   *and print the last digit of the number stored in the variable n
  *    *Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 int ch;
