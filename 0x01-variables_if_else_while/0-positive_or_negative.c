3290387a0a316b63de3f8a353b4a4a17b1c53bf8
yabsera <kidistkifleyohannes364@gmail.com>
2022-06-09 14:25:35 -0700

    zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 
-/*main -> assign a random number to the variable num each time it is executed*/
- /*and print the last digit of the number stored in the variable n*/
- /*Return: Always 0 (Success)*/
+/**
+ *  *main -> assign a random number to the variable n each time it is executed
+ *   *and print the last digit of the number stored in the variable n
+ *    *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int num;
+	int n;
 
 	srand(time(0));
-	num = rand() - RAND_MAX / 2;
-	if (num > 0)
-			printf("%d is positive\n", num);
-	if (num == 0)
-			printf("%d is zero\n", num);
-	if (num < 0)
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+			printf("%d is positive\n", n);
+	if (n == 0)
+			printf("%d is zero\n", n);
+	if (n < 0)
 	{
-			printf("%d is negative\n", num);
+			printf("%d is negative\n", n);
 	}
 	return (0);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,34 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)}
 	{
 		for (j = 0; j < 60; j++)
 		{
-			if (i < 10)
-			{																_putchar('0');														_putchar(i + '0');																											}
-																		else if (i >= 10)
-			{																_putchar((i / 10) + '0');												_putchar((i % 10) + '0');											}
-																		if (j < 10)							
-			{																_putchar(':');														_putchar('0');														_putchar(j + '0');
+			if(i < 10)
+			{
+				_putchar('0'):
+				_putchar(i + '0');
 			}
-			else if (j >= 10)													{																_putchar(':');
-				_putchar((j / 10) + '0');												_putchar((j % 10) + '0');											}
-			_putchar('\n');													}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (i < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j >= 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	
 	}
 }
